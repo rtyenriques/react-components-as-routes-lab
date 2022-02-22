2480519630bb68b6cf7d5d7676fbeee8c6b7c775
= <rtyenriques@gmail.com>
2022-02-22 01:09:59 -0800

    commit tree 447701faf00dd08e624b6d52612a9685b1b91d6b

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,13 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map(actor => (
+        <div>
+          {actor.name}
+          <ul>{actor.movies.map(movies => <li>{movies}</li>)}</ul>
+          </div>
+      ))}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,13 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map(movie => (
+          <div>{movie.title}
+          {movie.time} 
+          <ul>{movie.genres.map(genre => <li>{genre}</li>)}</ul>
+          </div>
+        ))}
     </div>
   );
 };
